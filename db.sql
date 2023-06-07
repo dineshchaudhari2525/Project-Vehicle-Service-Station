8bcdfb84339053e8839802f235d16f43a3ccc461
dineshchaudhari2525 <dineshchaudhari2525@gmail.com>
2023-06-07 12:58:29 +0530

    customer table created

---
+++ db.sql
@@ -0,0 +1,1 @@
+ create table customer_vehicles(vehicle_number VARCHAR(10) PRIMARY KEY,customer_id INT,vehicle_id INT, foreign key(customer_id) REFERENCES customer(id) ON DELETE CASCADE ON UPDATE CASCADE,foreign key(vehicle_id) REFERENCES vehicle(id) ON DELETE CASCADE ON UPDATE CASCADE);
