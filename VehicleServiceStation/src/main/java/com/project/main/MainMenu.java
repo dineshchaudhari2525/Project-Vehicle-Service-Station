b3f05c14af168a9b6aab46a29c466ee6b2077d93
dineshchaudhari2525 <dineshchaudhari2525@gmail.com>
2023-06-08 12:01:28 +0530

    MainMenu and SubMenu created

--- VehicleServiceStation/src/main/java/com/project/main/MainMenu.java
+++ VehicleServiceStation/src/main/java/com/project/main/MainMenu.java
@@ -1,5 +1,49 @@
 package com.project.main;
 
+import java.util.Scanner;
+
 public class MainMenu {
+	
+	public static int mainMenuOptions() {
+		System.out.println("0.Exit");
+		System.out.println("1.Customer");
+		System.out.println("2.Vehicle");
+		System.out.println("3.Service");
+		System.out.println("4.parts");
+		System.out.println("5.Today's Business");
+		System.out.println("6.Given Date's Business");
+		System.out.println("Enter your Choice: ");
+		return new Scanner(System.in).nextInt();
+	}
+	public static void main(String[] args) {
+		int choice;
+		while((choice=mainMenuOptions())!=0) {
+			switch (choice) {
+			case 1:
+				SubMenu.customerMain();
+				break;
+			case 2:
+				SubMenu.vehicleMain();
+				break;
+			case 3:
+				SubMenu.serviceMain();
+				break;
+			case 4:
+				SubMenu.partsMain();
+				break;
+			case 5:
+				SubMenu.todaysBusinessMain();
+				break;
+			case 6:
+				SubMenu.givenDateBusinessMain();
+				break;
+				
+			default:
+				System.out.println("Wrong Choice");
+				break;
+			}
+		}
+	}
+	
 
 }
---
+++ VehicleServiceStation/src/main/java/com/project/main/SubMenu.java
@@ -0,0 +1,192 @@
+package com.project.main;
+
+import java.util.Scanner;
+
+public class SubMenu {
+	
+	public static int customerMenu() {
+		System.out.println("0.Back");
+		System.out.println("1.Add Customer");
+		System.out.println("2.Display All Customer");
+		System.out.println("3.Display Specific Customer Details");
+		System.out.println("4.Edit Customer");
+		System.out.println("5.Delete Customer");
+		System.out.println("Enter Choice: ");
+		return new Scanner(System.in).nextInt();
+		}
+	   public static void customerMain() {
+		int choice;
+		while((choice=customerMenu())!=0) {
+			switch (choice) {
+			case 1:
+				System.out.println("1.Add Customer");
+				break;
+			case 2:
+				System.out.println("2.Display All Customer");
+				break;
+			case 3:
+				System.out.println("3.Display Specific Customer Details");
+				break;
+			case 4:
+				System.out.println("4.Edit Customer");
+				break;
+			case 5:
+				System.out.println("5.Delete Customer");
+				break;
+
+			default:
+				System.out.println("Wrong Choice");
+				break;
+			}
+		}
+	}
+	   public static int vehicleMenu() {
+			System.out.println("0.Back");
+			System.out.println("1.Add Vehicle");
+			System.out.println("2.Display All Vehicle");
+			System.out.println("3.Display Specific Vehicle Details");
+			System.out.println("4.Edit Vehicle");
+			System.out.println("5.Delete Vehicle");
+			System.out.println("Enter Choice: ");
+			return new Scanner(System.in).nextInt();
+			}
+		   public static void vehicleMain() {
+			int choice;
+			while((choice=vehicleMenu())!=0) {
+				switch (choice) {
+				case 1:
+					System.out.println("Add Vehicle");
+					break;
+				case 2:
+					System.out.println("Display All Vehicle");
+					break;
+				case 3:
+					System.out.println("Display Specific Vehicle Details");
+					break;
+				case 4:
+					System.out.println("Edit Vehicle");
+					break;
+				case 5:
+					System.out.println("Delete Vehicle");
+					break;
+
+				default:
+					System.out.println("Wrong Choice");
+					break;
+				}
+			}
+		}
+		   
+		   public static int serviceMenu() {
+				System.out.println("0.Back");
+				System.out.println("1.Select Customer Vehicle");
+				System.out.println("2.Process Request");
+				System.out.println("3.Prepare and Display Bill");
+				System.out.println("4.Get Payment Form Customer");
+				System.out.println("Enter Choice: ");
+				return new Scanner(System.in).nextInt();
+				}
+			   public static void serviceMain() {
+				int choice;
+				while((choice=serviceMenu())!=0) {
+					switch (choice) {
+					case 1:
+						System.out.println("Select Customer Vehicle");
+						break;
+					case 2:
+						SubMenu.processRequestMain();
+						break;
+					case 3:
+						System.out.println("Prepare and Display Bill");
+						break;
+					case 4:
+						System.out.println("Get Payment Form Customer");
+						break;
+
+					default:
+						System.out.println("Wrong Choice");
+						break;
+					}
+				}
+			}
+			   
+			   public static int processRequestMenu() {
+					System.out.println("0.Back");
+					System.out.println("1.New Services");
+					System.out.println("2.Existing Service ");
+					System.out.println("3.Maintenance");
+					System.out.println("4.Repairing");
+					System.out.println("5.Oil/Additive Change/Add ");
+					System.out.println("Enter Choice: ");
+					return new Scanner(System.in).nextInt();
+					}
+				   public static void processRequestMain() {
+					int choice;
+					while((choice=processRequestMenu())!=0) {
+						switch (choice) {
+						case 1:
+							System.out.println("New Services");
+							break;
+						case 2:
+							System.out.println("Existing Service");
+							break;
+						case 3:
+							System.out.println("Maintenance");
+							break;
+						case 4:
+							System.out.println("Repairing");
+							break;
+						case 5:
+							System.out.println("Oil/Additive Change/Add");
+							break;
+
+						default:
+							System.out.println("Wrong Choice");
+							break;
+						}
+					}
+				}
+			   public static int partsMenu() {
+					System.out.println("0.Back");
+					System.out.println("1.Add Part");
+					System.out.println("2.Display All Parts");
+					System.out.println("3.Edit Part Price");
+					System.out.println("4.Delete Part ");
+					System.out.println("Enter Choice: ");
+					return new Scanner(System.in).nextInt();
+					}
+				   public static void partsMain() {
+					int choice;
+					while((choice=partsMenu())!=0) {
+						switch (choice) {
+						case 1:
+							System.out.println("Add Part");
+							break;
+						case 2:
+							System.out.println("Display All Parts");
+							break;
+						case 3:
+							System.out.println("Edit Part Price");
+							break;
+						case 4:
+							System.out.println("Delete Part");
+							break;
+
+						default:
+							System.out.println("Wrong Choice");
+							break;
+						}
+					}
+				}
+				  public static void todaysBusinessMain() {
+					  System.out.println("Today's Business");
+					
+				}
+				  public static void givenDateBusinessMain() {
+					  System.out.println("given Date Business");
+					
+				}
+
+
+
+}
