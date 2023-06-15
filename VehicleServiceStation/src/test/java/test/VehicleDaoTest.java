585ac77d3db98a3f205850d564751a56b9183d80
dineshchaudhari2525 <dineshchaudhari2525@gmail.com>
2023-06-15 14:58:32 +0530

    testSpecificCustomerVehicles() case from VehicleDao tested successfully.

--- VehicleServiceStation/src/test/java/test/VehicleDaoTest.java
+++ VehicleServiceStation/src/test/java/test/VehicleDaoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import com.project.dao.CustomerDao;
 import com.project.dao.VehicleDao;
+import com.project.entity.CustomerVehicle;
 import com.project.entity.Vehicle;
 
 public class VehicleDaoTest {
@@ -22,5 +23,21 @@ public class VehicleDaoTest {
 		}
 		
 	}
+	@Test
+	public void testSpecificCustomerVehicles() {
+		VehicleDao vehicleDao;
+		try {
+			vehicleDao = new VehicleDao();
+			System.out.println(vehicleDao.specificCustomerVehicles(2));
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 
 }
+
+   
+//testAddVehicle() case from VehicleDao tested successfully
